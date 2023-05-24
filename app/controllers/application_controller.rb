fddf00904267e4f92c4dab746f8d7b2352e69f7e
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-05-24 16:32:15 -0400

    Verifying middleware setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+
+    # Verifying cookies and sessions middleware working
+    def hello_world
+        session[:count] = (session[:count] || 0) + 1
+        render json: { count: session[:count] }
+    end
+
 end
